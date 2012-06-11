2b6d72b54abe6499943616ff4d771d6944a27e4d
jordens@gmail.com <>
2012-06-11 16:05:58 -0600

    start 1.2+dev

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from glob import glob
 
 setup(
         name = "electrode",
-        version = "1.2",
+        version = "1.2+dev",
         author = "Robert Jordens",
         author_email = "jordens@phys.ethz.ch",
         url = "http://launchpad.net/electrode",
