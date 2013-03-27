fix polygons, add derivative names, add MultiPotentialObjective, add rotation back to PotentialObjective

electrode/electrode.py
@@ -222,11 +222,15 @@ class PixelElectrode(Electrode):
         p = np.array(p.value).ravel()
         return p, c
 
-    def split(self, thresholds=[.5**.5]):
+    def split(self, thresholds=[0]):
+        if thresholds is None:
+            threshold = sorted(np.unique(self.pixel_factors))
         ts = [-np.inf] + thresholds + [np.inf]
         eles = []
         for i, (ta, tb) in enumerate(zip(ts[:-1], ts[1:])):
             good = (ta <= self.pixel_factors) & (self.pixel_factors < tb)
+            if not np.any(good):
+                continue
             paths = [self.paths[j] for j in np.argwhere(good)]
             name = "%s_%i" % (self.name, i)
             eles.append(self.__class__(name=name, paths=paths,

electrode/pattern_constraints.py
@@ -20,9 +20,10 @@
 
 import numpy as np
 
-from traits.api import HasTraits, Array, Float, Int
+from traits.api import HasTraits, Array, Float, Int, List, Instance, Str
 
-from .utils import select_tensor, expand_tensor, rotate_tensor
+from .utils import (select_tensor, expand_tensor, rotate_tensor,
+        name_to_deriv)
 
 
 class Constraint(HasTraits):
@@ -63,13 +64,33 @@ class PatternRangeConstraint(Constraint):
 
 class PotentialObjective(Constraint):
     x = Array(dtype=np.float64, shape=(3,))
-    d = Int # derivative order
-    e = Int # derivative component
-    v = Float # value
+    derivative = Str # derivative name
+    value = Float # value
+    rotation = Array(dtype=np.float64, shape=(3, 3), value=np.identity(3))
 
     def objective(self, electrode, variables):
-        c = electrode.value(self.x, self.d)[0][self.e, :, 0]
-        return [(c, self.v)]
+        d, e = name_to_deriv(self.derivative)
+        c, = electrode.value(self.x, d)
+        c = c[..., 0]
+        if not np.allclose(self.rotation, np.identity(3)):
+            c = expand_tensor(c)
+            c = rotate_tensor(c, self.rotation, d)
+            c = select_tensor(c)
+        c = c[e]
+        return [(c, self.value)]
+
+
+class MultiPotentialObjective(Constraint):
+    components = List(Instance(PotentialObjective))
+    weights = Array(dtype=np.float64, shape=(None,))
+    value = Float
+
+    def objective(self, electrode, variables):
+        c = 0.
+        for wi, oi in zip(self.weights, self.components):
+            for ci, vi in oi.objective(electrode, variables):
+                c += wi*ci
+        return [(c, self.value)]
 
 
 class PotentialConstraint(Constraint):

electrode/polygons.py
@@ -39,11 +39,11 @@ def polygons_to_system(polygons):
         for pi in p:
             pi = geometry.polygon.orient(pi, 1)
             ext = np.zeros((len(pi.exterior.coords)-1, 3))
-            ext[:, :2] = pi.exterior.coords[:-1]
+            ext[:, :2] = np.array(pi.exterior.coords)[:-1, :2]
             e.paths.append(ext)
             for ii in pi.interiors:
                 int = np.zeros((len(ii.coords)-1, 3))
-                int[:, :2] = ii.coords[-2::-1]
+                int[:, :2] = np.array(ii.coords)[-2::-1, :2]
                 e.paths.append(int)
     return s
 