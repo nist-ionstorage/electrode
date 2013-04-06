b5e6731e2e9658484fd3d0840c226475f361a8fe
Robert Jordens <jordens@phys.ethz.ch>
2013-04-06 03:56:09 -0600

    another py3k fix

--- electrode/__init__.py
+++ electrode/__init__.py
@@ -1,4 +1,5 @@
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import (absolute_import, print_function,
+        unicode_literals, division)
 
 from .system import System
 from .electrode import (PolygonPixelElectrode, PointPixelElectrode,
