3408321dec718ea40cbaea60ba1ee07ecaad55b1
Robert Jordens <jordens@phys.ethz.ch>
2013-04-07 01:12:06 -0600

    nogil

--- electrode/cexpressions.pyx
+++ electrode/cexpressions.pyx
@@ -48,15 +48,16 @@ def point_potential(np.ndarray[dtype_t, ndim=2] x not None,
     if out is None:
         out = np.zeros([nx, derivative*2+1], dtype=dtype)
 
-    for i in range(nv):
-        a0 = areas[i]*potential
-        for j in range(nx):
-            x0 = x[j, 0] - points[i, 0]
-            y0 = x[j, 1] - points[i, 1]
-            for k in range(-cover_nmax, cover_nmax+1):
-                z0 = x[j, 2] + 2*k*cover_height
-                r0 = sqrt(x0**2 + y0**2 + z0**2)
-                point_potential_expr(x0, y0, z0, r0, a0, derivative, &out[j, 0])
+    with nogil:
+        for i in range(nv):
+            a0 = areas[i]*potential
+            for j in range(nx):
+                x0 = x[j, 0] - points[i, 0]
+                y0 = x[j, 1] - points[i, 1]
+                for k in range(-cover_nmax, cover_nmax+1):
+                    z0 = x[j, 2] + 2*k*cover_height
+                    r0 = sqrt(x0**2 + y0**2 + z0**2)
+                    point_potential_expr(x0, y0, z0, r0, a0, derivative, &out[j, 0])
     return out
 
 
@@ -77,20 +78,21 @@ def polygon_potential(np.ndarray[dtype_t, ndim=2] x not None,
     for i, polygon in enumerate(iter(polygons)):
         no = polygon.shape[0]
         assert polygon.shape[1] == 2
-        for j in range(nx):
-            for m in range(-cover_nmax, cover_nmax+1):
-                x2 = x[j, 0] - polygon[no-1, 0]
-                y2 = x[j, 1] - polygon[no-1, 1]
-                z = x[j, 2] + 2*m*cover_height
-                r2 = sqrt(x2**2 + y2**2 + z**2)
-                for k in range(no):
-                    x1, y1, r1 = x2, y2, r2
-                    x2 = x[j, 0] - polygon[k, 0]
-                    y2 = x[j, 1] - polygon[k, 1]
+        with nogil:
+            for j in range(nx):
+                for m in range(-cover_nmax, cover_nmax+1):
+                    x2 = x[j, 0] - polygon[no-1, 0]
+                    y2 = x[j, 1] - polygon[no-1, 1]
+                    z = x[j, 2] + 2*m*cover_height
                     r2 = sqrt(x2**2 + y2**2 + z**2)
-                    l2 = (x1 - x2)**2 + (y1 - y2)**2
-                    edge_potential_expr(x1, x2, y1, y2, r1, r2, l2, z,
-                            potential, derivative, &out[j, 0])
+                    for k in range(no):
+                        x1, y1, r1 = x2, y2, r2
+                        x2 = x[j, 0] - polygon[k, 0]
+                        y2 = x[j, 1] - polygon[k, 1]
+                        r2 = sqrt(x2**2 + y2**2 + z**2)
+                        l2 = (x1 - x2)**2 + (y1 - y2)**2
+                        edge_potential_expr(x1, x2, y1, y2, r1, r2, l2, z,
+                                potential, derivative, &out[j, 0])
     return out
 
 
@@ -114,25 +116,26 @@ def mesh_potential(np.ndarray[dtype_t, ndim=2] x not None,
     if out is None:
         out = np.zeros([nx, derivative*2+1], dtype=dtype)
 
-    for i in range(ne):
-        for j in range(nx):
-            for k in range(-cover_nmax, cover_nmax+1):
-                z = x[j, 2] + 2*k*cover_height
-                x1 = x[j, 0] - points[edges[i, 0], 0]
-                y1 = x[j, 1] - points[edges[i, 0], 1]
-                r1 = sqrt(x1**2 + y1**2 + z**2)
-                x2 = x[j, 0] - points[edges[i, 1], 0]
-                y2 = x[j, 1] - points[edges[i, 1], 1]
-                r2 = sqrt(x2**2 + y2**2 + z**2)
-                l2 = (x1 - x2)**2 + (y1 - y2)**2
-                potential = potentials[polygons[i]]
-                edge_potential_expr(x1, x2, y1, y2, r1, r2, l2, z,
-                        potential, derivative, &out[j, 0])
+    with nogil:
+        for i in range(ne):
+            for j in range(nx):
+                for k in range(-cover_nmax, cover_nmax+1):
+                    z = x[j, 2] + 2*k*cover_height
+                    x1 = x[j, 0] - points[edges[i, 0], 0]
+                    y1 = x[j, 1] - points[edges[i, 0], 1]
+                    r1 = sqrt(x1**2 + y1**2 + z**2)
+                    x2 = x[j, 0] - points[edges[i, 1], 0]
+                    y2 = x[j, 1] - points[edges[i, 1], 1]
+                    r2 = sqrt(x2**2 + y2**2 + z**2)
+                    l2 = (x1 - x2)**2 + (y1 - y2)**2
+                    potential = potentials[polygons[i]]
+                    edge_potential_expr(x1, x2, y1, y2, r1, r2, l2, z,
+                            potential, derivative, &out[j, 0])
     return out
 
 
 cdef inline void point_potential_expr(double x, double y, double z,
-        double r, double a, int derivative, double *d):
+        double r, double a, int derivative, double *d) nogil:
     cdef double n
     if derivative == 0:
         n = a/(2*M_PI*r**3)
@@ -186,7 +189,7 @@ cdef inline void point_potential_expr(double x, double y, double z,
 
 cdef inline void edge_potential_expr(double x1, double x2,
         double y1, double y2, double r1, double r2,
-        double l2, double z, double a, int derivative, double *d):
+        double l2, double z, double a, int derivative, double *d) nogil:
     cdef double zs, n
     if derivative == 0:
         zs = fabs(z)
