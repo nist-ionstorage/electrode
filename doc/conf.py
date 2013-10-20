b729e7cceaa4d215a0032194bb3a0f6615296271
Robert Jördens <jordens@gmail.com>
2013-10-19 19:08:16 -0600

    go for standard theme

--- doc/conf.py
+++ doc/conf.py
@@ -100,9 +100,10 @@ numpydoc_show_class_members = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+html_theme = 'default'
 #html_theme = 'nature'
-html_theme = 'sphinxdoc'
+#html_theme = 'sphinxdoc'
+#html_theme = 'agogo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
