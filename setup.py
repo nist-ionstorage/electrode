9afbf17758b933bf18da4547c6044f449aebd640
Robert Jordens <jordens@gmail.com>
2014-05-24 20:32:49 -0600

    start 1.5+dev

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ import numpy
 
 setup(
         name = "electrode",
-        version = "1.5",
+        version = "1.5+dev",
         author = "Robert Jordens",
         author_email = "jordens@gmail.com",
         url = "http://github.com/nist-ionstorage/electrode",
